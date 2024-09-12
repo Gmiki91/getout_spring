168a2f72b605e7dbe581859d592f8c32c109229a
gmiki91 <gmiki91@gmail.com>
2024-09-12 20:50:23 +0200

    delete event

--- src/main/java/com/blue/getout/event/EventController.java
+++ src/main/java/com/blue/getout/event/EventController.java
@@ -2,10 +2,8 @@ package com.blue.getout.event;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Map;
 
@@ -15,9 +13,13 @@ public class EventController {
     @Autowired
     EventService eventService;
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<Map<String, List<Event>>> getFilteredEvents(@PathVariable String userId){
         Map<String, List<Event>> result = eventService.getEventsForUser(userId);
         return ResponseEntity.ok(result);
+    }
+    @DeleteMapping("/{eventId}")
+    public void deleteEvent(@PathVariable String eventId){
+        eventService.deleteEvent(eventId);
     }
 }
--- src/main/java/com/blue/getout/event/EventService.java
+++ src/main/java/com/blue/getout/event/EventService.java
@@ -1,6 +1,7 @@
 package com.blue.getout.event;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -12,12 +13,21 @@ public class EventService {
     @Autowired
     EventRepository eventRepository;
 
-    public Map<String, List<Event>> getEventsForUser(String userId){
-       List<Event> joinedEvents =  eventRepository.findEventsJoinedByUser(userId);
-       List<Event> otherEvents = eventRepository.findEventsNotJoinedByUser(userId);
-        Map<String,List<Event>> result = new HashMap<>();
-        result.put("joinedEvents",joinedEvents);
-        result.put("otherEvents",otherEvents);
+    public Map<String, List<Event>> getEventsForUser(String userId) {
+        List<Event> joinedEvents = eventRepository.findEventsJoinedByUser(userId);
+        List<Event> otherEvents = eventRepository.findEventsNotJoinedByUser(userId);
+        Map<String, List<Event>> result = new HashMap<>();
+        result.put("joinedEvents", joinedEvents);
+        result.put("otherEvents", otherEvents);
         return result;
+    }
+
+    public void deleteEvent(String eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        event.getParticipants().forEach(user -> user.getJoinedEvents().remove(event));
+        event.getParticipants().clear();
+        eventRepository.save(event);
+        eventRepository.delete(event);
     }
 }
