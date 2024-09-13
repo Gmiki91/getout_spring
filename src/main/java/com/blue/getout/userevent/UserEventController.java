594d47f734de1a4f8564b2eeae2fca1737d5c5b8
gmiki91 <gmiki91@gmail.com>
2024-09-13 12:04:37 +0200

    leave event, not sending event back

--- src/main/java/com/blue/getout/userevent/UserEventController.java
+++ src/main/java/com/blue/getout/userevent/UserEventController.java
@@ -1,8 +1,6 @@
 package com.blue.getout.userevent;
 
-import com.blue.getout.event.Event;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +10,12 @@ public class UserEventController {
     @Autowired
     private UserEventService userEventService;
 
-    @PostMapping("/join/{userId}/{eventId}")
-    public ResponseEntity<Event> joinEvent(@PathVariable String userId, @PathVariable String eventId) {
-       return userEventService.joinEvent(userId,eventId);
+    @PostMapping("/{userId}/join/{eventId}")
+    public void joinEvent(@PathVariable String userId, @PathVariable String eventId) {
+        userEventService.joinEvent(userId,eventId);
+    }
+    @DeleteMapping("/{eventId}/participants/{userId}")
+    public void leaveEvent(@PathVariable String eventId, @PathVariable String userId){
+        userEventService.leaveEvent(eventId,userId);
     }
 }
--- src/main/java/com/blue/getout/userevent/UserEventService.java
+++ src/main/java/com/blue/getout/userevent/UserEventService.java
@@ -5,7 +5,7 @@ import com.blue.getout.event.EventRepository;
 import com.blue.getout.user.User;
 import com.blue.getout.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +17,7 @@ public class UserEventService {
     @Autowired
     private EventRepository eventRepository;
 
-    public ResponseEntity<Event> joinEvent(String userId, String eventId) {
+    public void joinEvent(String userId, String eventId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         Event event = eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found"));
 
@@ -26,6 +26,19 @@ public class UserEventService {
 
         userRepository.save(user);
         eventRepository.save(event);
-        return ResponseEntity.ok(event);
+    }
+
+    public void leaveEvent(String eventId, String userId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        boolean userRemoved = event.getParticipants().remove(user);
+        boolean eventRemoved = user.getJoinedEvents().remove(event);
+        if (userRemoved && eventRemoved) {
+            eventRepository.save(event);
+        } else {
+            throw new IllegalArgumentException("User not found in the participants list");
+        }
     }
 }
