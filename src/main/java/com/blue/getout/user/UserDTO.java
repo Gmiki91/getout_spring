087fb6f918854410ebce4013f31a5233383fe897
Miklos Gabor <gmiki91@gmail.com>
2025-04-30 16:08:02 +0200

    respond with email for login

--- src/main/java/com/blue/getout/user/UserDTO.java
+++ src/main/java/com/blue/getout/user/UserDTO.java
@@ -5,4 +5,4 @@ import com.blue.getout.notification.NotificationDTO;
 import java.util.Set;
 import java.util.UUID;
 
-public record UserDTO (UUID id, String name, String avatarUrl, Set<NotificationDTO> notifications){}
+public record UserDTO (UUID id, String name,String email, String avatarUrl, Set<NotificationDTO> notifications){}
